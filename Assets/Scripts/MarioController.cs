8d4613e358755f53d2a745f5acff319ed394c50e
1612542 <tnquoc1998@gmail.com>
2019-09-25 21:05:10 +0700

    1612542-A01: Project Game Unity 2D

---
+++ Assets/Scripts/MarioController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioController : MonoBehaviour
+{
+    Vector2 pos;
+    float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                pos.x += speed;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                pos.x -= speed;
+            }
+            transform.position = pos;
+            GetComponent<Animator>().SetBool("isRunning", true);
+        }
+        else
+        {
+            //transform.position = pos;
+            GetComponent<Animator>().SetBool("isRunning", false);
+        }
+    }
+}
